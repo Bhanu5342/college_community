13cbce63482561444cb568250e5376e72c9a0258
Bhanu5342 <147545754+Bhanu5342@users.noreply.github.com>
2025-04-01 11:36:27 +0000

    added login

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import login from './pages/login';
+
+function App() {
+  return (
+    <login/>
+  );
+}
+
+export default App;
---
+++ my-app/src/pages/login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function login(){
+    return (
+    <div class="card">
+        <div class="card-body">
+          This is some text within a card body.
+        </div>
+      </div>
+
+    )
+}
+
+export default login;
